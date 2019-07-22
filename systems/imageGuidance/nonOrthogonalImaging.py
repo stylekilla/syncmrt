Some testing. Fixed error with 0.0 values in point calculation.

systems/imageGuidance/nonOrthogonalImaging.py
@@ -39,10 +39,11 @@ def calculate(p1,p2,t1,t2):
 			alpha = np.deg2rad(45-t2)
 			beta = np.deg2rad(t1+45)
 			# Unpack the two points.
-			# a, z1 = p1
-			# b, z2 = p2
 			a, z2 = p2[i]
 			b, z1 = p1[i]
+			# Do some safe conversions if zeros are encountered.
+			if a == 0: a = 1e-9
+			if b == 0: b = 1e-9
 			# Calculate the separation of each imaging axis from the true X axis.
 			phi = np.pi/2 - alpha - beta
 			psi_a = np.arctan((a*np.sin(phi))/(a*np.cos(phi)+b))

systems/imageGuidance/solver.py
@@ -20,7 +20,7 @@ class solver:
 		self.solution = np.zeros((6,))
 		self.transform = np.identity(4)
 
-	def input(self,left=None,right=None,patientIsoc=None,machineIsoc=None):
+	def setInputs(self,left=None,right=None,patientIsoc=None,machineIsoc=None):
 		# Update vars.
 		if left is not None: self._leftPoints = np.array(left)
 		if right is not None: self._rightPoints = np.array(right)
@@ -44,9 +44,9 @@ class solver:
 		if self._patientIsocenter is None:
 			self._patientIsocenter = self._leftCentroid
 
-		print('Left Points:',self._leftPoints)
+		print('Left Points:\n',self._leftPoints)
 		print('Left Ctd:',self._leftCentroid)
-		print('Right Points:',self._rightPoints)
+		print('Right Points:\n',self._rightPoints)
 		print('Right Ctd:',self._rightCentroid)
 		print('Patient Isoc:',self._patientIsocenter)
 		print('Machine Isoc:',self._machineIsocenter)
@@ -213,7 +213,6 @@ def angles(R):
 				del z[i]
 			except:
 				logging.critical('Cannot solve alignment. Unknown cause.')
-				print('\033[91m Unable to solve for the alignment. Please select the points properly.')
 				return 0, 0, 0
 
 	# Angles must be applied in xyz order.