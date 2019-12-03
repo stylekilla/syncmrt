Fixed marker gathering from plots and order of points for calculation. Also fixed gui links to alignment calcs.

QsWidgets/QsWorkspace.py
@@ -101,16 +101,17 @@ class QPlotEnvironment(QtWidgets.QSplitter):
 		markers1 = np.array(self.tableModel[1].getMarkers())
 		if raw:
 			# If we want just the raw points, take them.
-			points[:,0] = markers0[:,0]
+			# We assume that points is [x,y,z] and marker0 is [y,z] and marker1 is [x,z].
+			points[:,1] = markers0[:,0]
 			# See if we have two sets of points.
 			allZeroes = not markers1.any()
 			if allZeroes:
 				# Only one dataset, take that only.
-				points[:,1] = markers0[:,1]
+				points[:,2] = markers0[:,1]
 			else:
 				# Reconcile both datasets.
-				points[:,1] = (markers0[:,1] + markers1[:,1])/2
-				points[:,2] = markers1[:,2]
+				points[:,2] = (markers0[:,1] + markers1[:,1])/2
+				points[:,0] = markers1[:,0]
 		else:
 			# Else, adjust the points for the views.
 			theta0 = self.plotView[0]

main.py
@@ -73,9 +73,9 @@ class main(QtWidgets.QMainWindow, Ui_MainWindow):
 		# Sidebar: Alignment.
 		self.sbAlignment = self.sidebar.addPage('Alignment',QsWidgets.QAlignment(),after='Imaging')
 		self.sbAlignment.widget['maxMarkers'].valueChanged.connect(partial(self.updateSettings,'global',self.sbAlignment.widget['maxMarkers']))
-		self.sbAlignment.widget['calcAlignment'].clicked.connect(partial(self.patientCalculateAlignment,index=-1))
-		self.sbAlignment.widget['doAlignment'].clicked.connect(partial(self.patientApplyAlignment,index=-1))
 		self.sbAlignment.widget['optimise'].toggled.connect(partial(self.toggleOptimise))
+		self.sbAlignment.calculateAlignment.connect(self.patientCalculateAlignment)
+		self.sbAlignment.doAlignment.connect(self.patientApplyAlignment)
 		# Add treatment section to sidebar.
 		self.sidebar.addPage('Treatment',QsWidgets.QTreatment(),after='Alignment')
 		self.sbTreatment = self.sidebar.getPage('Treatment')
@@ -153,7 +153,7 @@ class main(QtWidgets.QMainWindow, Ui_MainWindow):
 		self.testing()
 
 	def testing(self):
-		# self.openXray('../scratch/test.hdf5')
+		self.openXray('../scratch/test.hdf5')
 
 		folder = '../scratch/DICOM/SMRT_CT_ONLY/'
 		dataset = []
@@ -165,31 +165,18 @@ class main(QtWidgets.QMainWindow, Ui_MainWindow):
 		if len(dataset) > 0:
 			self.openCT(dataset)
 
-	@QtCore.pyqtSlot(int)
-	def calculateAlignment(self,index):
-		print('TADA MAIN.py L:188')
-
-	@QtCore.pyqtSlot(float,float,float)
-	def ctUpdateIsocenter(self,x,y,z):
-		# Update the ct isocenter.
-		try:
-			self.patient.ct.isocenter = np.array([x,y,z])
-			self.patient.ct.plot.updatePatientIsocenter(self.patient.ct.isocenter)
-			logging.debug('Updated patient CT isocenter with vals: {} {} {}'.format(x,y,z))
-		except:
-			logging.warning('Unable to update CT isocenter.')
-
-	@QtCore.pyqtSlot(str)
-	def setControlsComplexity(self,level):
-		self.controls.setLevel(level)
-
-	@QtCore.pyqtSlot(bool)
-	def enableDoAlignment(self,state=False):
-		# self.pps._isStageConnected
-		self.sbAlignment.widget['doAlignment'].setEnabled(state)
-
-	def setControlsReadOnly(self,state):
-		self.controls.setReadOnly(bool(not state))
+		self.envXray.addMarker(0,0,0)
+		self.envXray.addMarker(0,50,25)
+		self.envXray.addMarker(0,-50,-25)
+		self.envXray.addMarker(1,0,0)
+		self.envXray.addMarker(1,50,25)
+		self.envXray.addMarker(1,-50,-25)
+		self.envCt.addMarker(0,0,0)
+		self.envCt.addMarker(0,50,25)
+		self.envCt.addMarker(0,-50,-25)
+		self.envCt.addMarker(1,0,0)
+		self.envCt.addMarker(1,50,25)
+		self.envCt.addMarker(1,-50,-25)
 
 	def newFile(self,modality):
 		if modality == 'xray':
@@ -584,7 +571,6 @@ class main(QtWidgets.QMainWindow, Ui_MainWindow):
 		# error = QtWidgets.QErrorMessage()
 		# error.showMessage("Please ensure {} markers are selected in the CT images.".format(numberOfPoints))
 		# return
-
 		if index == 0:
 			# Align to a CT.
 			# Get the x-ray (right) points. These are always in terms of the fixed synchrotron axes.

systems/imageGuidance/nonOrthogonalImaging.py
@@ -25,7 +25,7 @@ def calculate(p1,p2,t1,t2):
 		p
 			The calculated location of the object with respect to the fixed synchrotron XYZ axes.
 	"""
-	if not isinstance(p1,float):
+	if isinstance(p1,float):
 		# If a single number is passed then turn it into a numpy array.
 		p1 = np.array([p1])
 		p2 = np.array([p2])
@@ -35,7 +35,6 @@ def calculate(p1,p2,t1,t2):
 
 	result = np.zeros((len(p1),3))
 
-	# if t1 > t2:
 	if t1 > t2:
 		QtWidgets.QMessageBox.warning("The calculation will fail since the first image angle is greater than the second. The first image angle must always be less than the second, i.e. t1 = -45 and t2 = +45.")
 		return