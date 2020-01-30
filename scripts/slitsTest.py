727c16fcb95e60b905fee12c247291a38c051c71
Micah Barnes <micah.jamiel@gmail.com>
2020-01-30 12:00:20 +1100

    Updates to slit algorithm.

--- scripts/slitsTest.py
+++ scripts/slitsTest.py
@@ -15,25 +15,22 @@ contour = np.array([(np.cos(2*np.pi/n*x)*r,np.sin(2*np.pi/n*x)*r) for x in range
 
 # I need to give a list of points in X/Y and the tangent condition required by each point.
 gradientConditions = []
-gradientPositions = []
 
 # So if I take my contour, and resample it without loss:
-granularity = 20
+granularity = 1
 
 iterate = list(range(0,len(contour),granularity))
 for i in range(len(iterate)-1):
 	a = iterate[i]
 	b = iterate[i+1]
-	gradientConditions.append(contour[b,:] - contour[a,:])
-	gradientPositions.append(contour[int((a+b)/2),:])
+	x1,y1 = contour[a,:]
+	x2,y2 = contour[b,:]
+	gradientConditions.append([(x1+x2)/2,(y1+y2)/2,(x2-x1)/(y2-y1)])
 
-print((gradientConditions))
-print((gradientPositions))
-
-
-success = slitOptimister(speed,positions,gradientConditions)
+# success = slitOptimister(speed,positions,gradientConditions)
 # success = True if it all passed.
 # success = {'speed': True, 'acceleration': False}
 # If acceleration is too much then we need to know in which direction.
 # To reduce the acceleration we need to blur it out a little bit, so give a little bit on the positions.
 
+np.savetxt('gradientConditions.txt',gradientConditions,header='x,y,dx/dy',delimiter=',')
