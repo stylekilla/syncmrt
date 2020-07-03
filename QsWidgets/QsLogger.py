bf7e1c0c094794fd817a424f2b98a14521ea6a51
Micah Barnes <micah.jamiel@gmail.com>
2020-07-03 14:46:47 +1000

    Log into qsplitter. Extending log widget to allow for extra buttons in future.

--- QsWidgets/QsLogger.py
+++ QsWidgets/QsLogger.py
@@ -13,47 +13,77 @@ class main(QtWidgets.QMainWindow):
 		logger.addHandler(logWidget.handler)
 		logger.setLevel(logging.INFO)
 		logging.critical("Test critical message.")
+
+
 """
 
 __all__ = ['QLog']
 
 coloredlogs.install()
 
-class QLogHandler(logging.Handler,QtCore.QObject):
-	appendHtml = QtCore.pyqtSignal(str)
+# Remove matplotlib warnings.
+logging.info("Removing matplotlib logs.")
+logging.getLogger("matplotlib").setLevel(logging.CRITICAL)
+logging.getLogger("backend_managers").setLevel(logging.CRITICAL)
 
-	def __init__(self,textOutputWidget):
+class QLogHandler(logging.Handler,QtCore.QObject):
+	appendLog = QtCore.pyqtSignal(str)
+
+	def __init__(self,masterWidget):
 		super().__init__()
 		QtCore.QObject.__init__(self)
 		# The output widget.
-		self.widget = textOutputWidget
-		# Set to read only.
-		self.widget.setReadOnly(True)
+		self.widget = masterWidget
 		# Signal connections (thread safe).
-		self.appendHtml.connect(self.widget.appendHtml)
+		self.appendLog.connect(self.widget.appendLog)
 		# Set up the log style.
 		self.setFormatter(coloredlogs.ColoredFormatter("%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s"))
+		# Set the level.
+		self.setLevel(logging.WARNING)
 
 	def emit(self,record):
 		# Format the record.
 		record = self.format(record)
 		record = converter.convert(record, code=True, tabsize=4)
 		# Add the record to the widget.
-		self.appendHtml.emit(record)
+		self.appendLog.emit(record)
 
-class QLog(QtWidgets.QPlainTextEdit):
+class QLog(QtWidgets.QWidget):
 	def __init__(self,frame=None):
 		super().__init__()
+		# Make the widgets.
+		self.w = {}
+		self.w['title'] = QtWidgets.QLabel('Log Output')
+		self.w['title'].setStyleSheet("QLabel { background-color : #323232;}")
+		self.w['display'] = QtWidgets.QPlainTextEdit()
+		self.w['display'].setReadOnly(True)
+
 		# Make a handler for the logs.
 		self.handler = QLogHandler(self)
 
-		# Install logger into the allocated frame.
+		# Create our layout of widgets.
+		layout = QtWidgets.QVBoxLayout()
+		layout.setContentsMargins(0,0,0,0)
+		layout.addWidget(self.w['title'])
+		layout.addWidget(self.w['display'])
+		self.setLayout(layout)
+
+		# Install logger into the allocated frame (sent into this constructor method).
 		self.frame = frame
+
+		# Set ourselves as the widget in the frame.
 		if self.frame is not None:
-			layout = QtWidgets.QHBoxLayout()
-			layout.addWidget(self)
-			layout.setContentsMargins(0,0,0,0)
-			self.frame.setLayout(layout)
+			frameLayout = QtWidgets.QHBoxLayout()
+			frameLayout.setContentsMargins(0,0,0,0)
+			frameLayout.addWidget(self)
+			self.frame.setLayout(frameLayout)
+
+		# Set default to invisibile.
+		self.frame.setVisible(False)
+
+	def appendLog(self,*args,**kwargs):
+		# Append the log.
+		self.w['display'].appendHtml(*args,**kwargs)
 
 	def toggleVisibility(self):
 		'''Show/hide frane as requested.'''
