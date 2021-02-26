760eb3a3bb709604c762497820bd74878046efc4
Micah Barnes <micah.jamiel@gmail.com>
2021-02-26 21:08:05 +1100

    Two new scripts for dynamic imaging. Edits for detector to match.

---
+++ scripts/stitchHDF.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py as hdf
+
+imagedir = '/mnt/tmp/'
+filename = 'UUID3.hdf'
+
+f = hdf.File(imagedir+filename)
+arr = np.vstack(f['entry']['data']['data'])
+# arr = np.average(f['entry']['data']['data'],axis=1)
+f.close()
+
+fig,ax = plt.subplots(1)
+# extent = [0,arr.shape[1],arr.shape[0]*5,0]
+# ax.imshow(arr,extent=extent)
+ax.imshow(arr)
+plt.show()
