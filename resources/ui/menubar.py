42fa00ab073c498e60010f6c0be95a376ad48416
Micah Barnes <micah.jamiel@gmail.com>
2021-04-27 10:00:30 +1000

    Menu shortcut changes.

--- resources/ui/menubar.py
+++ resources/ui/menubar.py
@@ -11,13 +11,13 @@ def populate(mb):
 	items['new_xray'].setShortcut('Ctrl+N')
 	# LOAD
 	file_load = file.addMenu("Load")
-	items['load_xray'] = file_load.addAction("X-Ray")
-	items['load_ct'] = file_load.addAction("CT")
-	items['load_rtplan'] = file_load.addAction("RT Plan")
+	items['load_xray'] = file_load.addAction("&X-Ray")
+	items['load_xray'].setShortcut('Ctrl+L')
+	items['load_ct'] = file_load.addAction("&CT")
+	items['load_rtplan'] = file_load.addAction("RT &Plan")
 	items['load_syncplan'] = file_load.addAction("Synchrotron Treatment Plan")
 	file_load.addSeparator()
-	items['load_folder'] = file_load.addAction("Folder")
-	items['load_folder'].setShortcut('Ctrl+L')
+	items['load_folder'] = file_load.addAction("&Folder")
 
 	"""
 	TOOLS
